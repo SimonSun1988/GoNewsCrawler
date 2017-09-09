e62b96e9125ab49482f5a2b8ac95216cd4fbccd2
Simon Sun <blackghost0118@gmail.com>
2017-09-09 23:52:16 +0800

    add cron job and add appledaliy news

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
     "gopkg.in/mgo.v2"
     "gopkg.in/mgo.v2/bson"
     "time"
+    "github.com/robfig/cron"
 )
 
 type News struct {
@@ -35,8 +36,16 @@ func Parallelize(functions ...func()) {
 }
 
 func main() {
+    cronjob := cron.New()
+    spec := "0 */3 * * * *"
+    cronjob.AddFunc(spec, func() {
+        Parallelize(ettoday, appledaily)
+    })
+    cronjob.Start()
+    select {}
+
     // Parallelize(ettoday, appledaily)
-    ettoday()
+    // ettoday()
     // appledaily()
 }
 
@@ -103,16 +112,54 @@ func ettoday() {
 }
 
  func appledaily() {
+
+    // 開啟 mongodb connection，並且在 function 結束後關閉
+    session, err := mgo.Dial("mongodb://localhost:27017")
+    defer session.Close()
+    // 連線到 all_news db 與 news collection
+    newsCollection := session.DB("all_news").C("news")
+
     doc, err := goquery.NewDocument("http://www.appledaily.com.tw/realtimenews/section/new/")
 
     if err != nil {
       log.Fatal(err)
     }
 
-    doc.Find(".item").Each(func(i int, s *goquery.Selection) {
-        // date := s.Find("span").Text()
-        title, _ := s.Find("img").Attr("alt")
+    doc.Find("li.rtddt").Each(func(i int, s *goquery.Selection) {
         url, _ := s.Find("a").Attr("href")
-        fmt.Printf("Review %d: %s - %s - %s\n", i+1, url, title)
+
+        innerNews, err := goquery.NewDocument("http://www.appledaily.com.tw" + url)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        title := innerNews.Find("#h1").Text()
+        image, _ := innerNews.Find(".imgmid2 img").Attr("src")
+        dateString := innerNews.Find(".gggs time").Text()
+        date, _ := time.Parse("2006年01月02日15:04", dateString)
+
+        // 用新聞連結找出資料
+        result := News{}
+        _ = newsCollection.Find(bson.M{"url": "http://www.appledaily.com.tw" + url}).One(&result)
+
+        if result.Url != "" {
+            fmt.Printf("已經存在的新聞資料: %s\n", "http://www.appledaily.com.tw" + result.Url)
+            fmt.Printf("時間: %s\n", result.CreatedAt)
+            fmt.Println("------------------------\n")
+        }
+
+        // 如果找不到這個新聞連結的資料，就幫他建立新的資料
+        if result.Url == "" {
+            fmt.Printf("建立新的新聞資訊: %s\n", "http://www.appledaily.com.tw" + url)
+            fmt.Printf("時間: %s\n", date)
+            fmt.Println("------------------------\n")
+            _ = newsCollection.Insert(&News{
+                Provider: "appledaily",
+                Title: title,
+                Image: image,
+                Url: "http://www.appledaily.com.tw" + url,
+                CreatedAt: date,
+            })
+        }
     })
  }
